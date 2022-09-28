8697a714e8c5f20005615cecbe45776bfcc62f5b
Y-a-s-h-b <yash.bansal2021@vitbhopal.ac.in>
2022-09-28 13:06:41 +0530

    UI

---
+++ Platformer/Assets/ItemCollector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollector : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
