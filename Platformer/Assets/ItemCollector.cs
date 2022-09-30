2a59c1453aa6d8d1362f4c9354db51da08dd88ac
Y-a-s-h-b <yash.bansal2021@vitbhopal.ac.in>
2022-09-30 16:31:16 +0530

    UI

--- Platformer/Assets/ItemCollector.cs
+++ Platformer/Assets/ItemCollector.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
+    [SerializeField] private Text coinText;
+    private int coin = 0;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Coin"))
         {
             Destroy(collision.gameObject);
+            coin++;
+            coinText.text = "Coin: " + coin;
         }
     }
 
