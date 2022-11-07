491a577f1572118bb037a87294bf79d126edaa8e
Y-a-s-h-b <yash.bansal2021@vitbhopal.ac.in>
2022-11-07 22:27:27 +0530

    New background

--- Platformer/Assets/Scripts/Finish.cs
+++ Platformer/Assets/Scripts/Finish.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     private AudioSource finishSound;
+    private bool levelCompleted = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +15,16 @@ public class Finish : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Man")
+        if (collision.gameObject.name == "Man" && !levelCompleted)
         {
             finishSound.Play();
+            levelCompleted = true;
+            Invoke("CompleteLevel", 4f);
+            CompleteLevel();
         }
     }
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
